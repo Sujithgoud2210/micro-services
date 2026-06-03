1c4ef397295e7d7b8502410a3b3c2dbbaade8ddb
sujithgoud2210 <sujith200322@gmail.com>
2026-06-03 11:36:22 +0530

    intial commit

---
+++ eureka-service/src/test/java/com/springworks/eureka_service/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springworks.eureka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
